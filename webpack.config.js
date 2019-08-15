38704292f716b861dbed157d8f5435eefdef7bfc
Aleksey Rezvov <aleksey.rezvov@gmail.com>
2019-08-15 09:17:16 +0300

    Fix verification for Yandex Webmaster.

--- webpack.config.js
+++ webpack.config.js
@@ -122,13 +122,10 @@ module.exports = {
                 collapseWhitespace: true
             }
         }),
-        new HtmlWebpackPlugin({
-            filename:'yandex_eba83ee651ca233d.html',
-            template: 'src/html/views/yandex_eba83ee651ca233d.html',
-            minify: {
-                collapseWhitespace: true
-            }
-        }),
+        new CopyPlugin([{
+            to: 'yandex_eba83ee651ca233d.html',
+            from: 'src/html/yandex_eba83ee651ca233d.html',
+        }]),
         new MiniCssExtractPlugin({
         }),
         new webpack.ProvidePlugin({
